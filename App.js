036ca3c76aa9143f231b6ff4b9cd007fb74a5535
01LEE <tkqqkrrhf11@gmail.com>
2025-04-05 12:34:04 +0900

    navigation.navigate를 이용한 화면간 전환

--- App.js
+++ App.js
@@ -1,23 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 // react native navigation을 사용하는 데 필요한 import 
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer, useNavigation } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 
-function HomeScreen() {
+function HomeScreen() { 
+  const navigation = useNavigation(); // 복잡할 경우 사용할 수 있는 hook 
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text style={{ fontSize: 40, fontWeight: 'bold' }}>메인 화면</Text>
+      <Button
+        title='Go to Detail'
+        onPress={() => navigation.navigate('Detail')}>
+      </Button>
     </View>
   );
 }
 
-function DetailScreen() {
+function DetailScreen({ navigation }) { // 복잡하지 않기에 그냥 매개변수 자체로 navigation을 사용한다
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text style={{ fontSize: 40, fontWeight: 'bold' }}>상세보기 화면</Text>
+      
+      <Button title='Go to Home' onPress={() => navigation.navigate('Home')} />
+      <Button title='Go to Detail' onPress={() => navigation.push('Detail')} />
+
+
+
+      
     </View>
   );
 }
@@ -26,7 +38,7 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Detail">
+      <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Detail" component={DetailScreen} />
       </Stack.Navigator>
