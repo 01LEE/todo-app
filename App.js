5eecaad1f2fcfff85f1dd20f2375def52ec2d33b
01LEE <tkqqkrrhf11@gmail.com>
2025-04-05 14:50:49 +0900

    매개변수나 파람으로 값을 받아서 뿌리는 것 까지 적용

--- App.js
+++ App.js
@@ -1,35 +1,82 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput, Pressable } from 'react-native';
+import { useState } from 'react';
+
 
 // react native navigation을 사용하는 데 필요한 import 
 import { NavigationContainer, useNavigation } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 
-function HomeScreen() { 
+function HomeScreen() {
   const navigation = useNavigation(); // 복잡할 경우 사용할 수 있는 hook 
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text style={{ fontSize: 40, fontWeight: 'bold' }}>메인 화면</Text>
       <Button
         title='Go to Detail'
-        onPress={() => navigation.navigate('Detail')}>
-      </Button>
+        onPress={() => navigation.navigate('Detail')} />
+
+      <Button
+        title='Go to TodoWrite'
+        onPress={() => navigation.navigate('TodoWrite')}
+      />
     </View>
   );
 }
 
-function DetailScreen({ navigation }) { // 복잡하지 않기에 그냥 매개변수 자체로 navigation을 사용한다
+const TodoWriteScreen = ({ navigation, route }) => {
+  const [todos, setTodos] = useState('');
+
+  return (
+    <>
+      <TextInput
+        onChangeText={setTodos}
+        value={todos}
+        placeholder="할 일을 작성해주세요."
+        style={{
+          flex: 0.5,
+          pading: 10,
+          backgroundColor: '#fff',
+          borderRadius: 10,
+          margin: 10,
+          borderWidth: 2
+        }}
+      />
+      <Pressable onPress={() => {
+        navigation.navigate('Detail', { todos });
+        setTodos('');
+      }}>
+        <Text
+          style={{
+            width: "30%",
+            pading: 10,
+            backgroundColor: '#fff',
+            borderRadius: 10, margin: 10,
+            borderWidth: 2,
+            textAlign: 'center',
+            fontWeight: 'bold'
+          }}
+        >작성</Text>
+      </Pressable>
+    </>
+  );
+}
+
+function DetailScreen({ navigation, route }) { // 복잡하지 않기에 그냥 매개변수 자체로 navigation을 사용한다
+  const todos = route?.params?.todos ? route.params.todos : '작성된 내용이 없습니다.';
+
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text style={{ fontSize: 40, fontWeight: 'bold' }}>상세보기 화면</Text>
-      
+      <Text>작성 내용 : {todos}</Text>
+
       <Button title='Go to Home' onPress={() => navigation.navigate('Home')} />
       <Button title='Go to Detail' onPress={() => navigation.push('Detail')} />
 
 
 
-      
+
     </View>
   );
 }
@@ -41,6 +88,7 @@ export default function App() {
       <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Detail" component={DetailScreen} />
+        <Stack.Screen name="TodoWrite" component={TodoWriteScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
