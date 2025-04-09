280ec6d51a640c48cf865748e099a8792e6f0032
01LEE <tkqqkrrhf11@gmail.com>
2025-04-09 11:35:41 +0900

    TodoWriteScreen UI 수정 및 작성 및 취소 후 리스트 및 홈으로 이동

--- screens/TodoWriteScreen.js
+++ screens/TodoWriteScreen.js
@@ -2,41 +2,68 @@ import { StyleSheet, Text, View, Button, TextInput, Pressable } from 'react-nati
 import React, { useState } from 'react';
 
 const TodoWriteScreen = ({ navigation, route }) => {
-  const [todos, setTodos] = useState('');
+    const [todos, setTodos] = useState('');
 
-  return (
-    <>
-      <TextInput
-        onChangeText={setTodos}
-        value={todos}
-        placeholder="할 일을 작성해주세요."
-        style={{
-          flex: 0.5,
-          pading: 10,
-          backgroundColor: '#fff',
-          borderRadius: 10,
-          margin: 10,
-          borderWidth: 2
-        }}
-      />
-      <Pressable onPress={() => {
-        navigation.navigate('Detail', { todos });
-        setTodos('');
-      }}>
-        <Text
-          style={{
-            width: "30%",
-            pading: 10,
-            backgroundColor: '#fff',
-            borderRadius: 10, margin: 10,
-            borderWidth: 2,
-            textAlign: 'center',
-            fontWeight: 'bold'
-          }}
-        >작성</Text>
-      </Pressable>
-    </>
-  );
+    return (
+        <>
+            <TextInput
+                onChangeText={setTodos}
+                value={todos}
+                placeholder="할 일을 작성해주세요."
+                style={ style.inputBox}
+            />
+            <View style={{
+                flexDirection: 'row',
+                gap: 5,
+                justifyContent: 'center',
+                marginLeft: 10,
+                marginRight: 10
+            }}>
+                <Pressable
+                    style={style.PressableBtn}
+                    onPress={() => {
+                        navigation.navigate('TodoList', { todos });
+                        setTodos('');
+                    }}>
+                    <Text style={style.text}>작성</Text>
+                </Pressable>
+                <Pressable
+                    style={style.PressableBtn}
+                    onPress={() => {
+                        navigation.navigate('Home', { todos });
+                    }}>
+                    <Text >취소</Text>
+                </Pressable>
+            </View>
+
+        </>
+    );
 }
 
+const style = StyleSheet.create({
+    inputBox: {
+        minHeight: 200,
+        pading: 10,
+        backgroundColor: '#fff',
+        borderRadius: 10,
+        borderWidth: 2,
+        margin: 10,
+        fontSize: 20,
+        fontWeight: 'bold'
+    },
+    PressableBtn: {
+        width: "50%",
+        pading: 10,
+        backgroundColor: '#fff',
+        borderRadius: 10,
+        borderWidth: 2,
+        alignItems: 'center',
+        justifyContent: 'center',
+        fontWeight: 'bold',
+    },
+    text: {
+        fontSize: 20,
+        fontWeight: 'bold'
+    }
+})
 export default TodoWriteScreen
